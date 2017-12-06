0ba54472b41ac8d1e998870794c88ac9932f92b0
Michael Hug <Michael.Hug@Fiserv.com>
2017-12-06 11:31:41 -0500

    javascript empty string check

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -2,51 +2,55 @@ $('#create-user-btn').click(function() {
     var username=$('#username').val();
     var getURL="api/userSpacePermissionCheck/" + username;
     var putURL="api/createOrgUserSpacePermissions/" + username;
-    $('.response-body').html('Please wait ...');
-    $.ajax({
-        type: 'GET',
-        url: getURL,
-        success: function(result){
-            if(!result.isLDAPUserPresent) {
-                $('.response-body').html('This username was not found. Please enter a valid shortname.');
-            } else {
-                $.ajax({
-                    type: 'PUT',
-                    url: putURL,
-                    success: function(resultt) {
-                        $('.response-body').html('Your user was created!<br>');
-                        $('.response-body').append('<div class="user-info">'+
-                                                        '<div class="left"></div>'+
-                                                        '<div class="center"></div>'+
-                                                        '<div class="right"></div>'+
-                                                   '</div>');
-                        
-                        $('.user-info .left').append('<div>API endpoint</div>');
-                        $('.user-info .left').append('<div>Org</div>');
-                        $('.user-info .left').append('<div>Space</div>');
-                        $('.user-info .left').append('<div>Username</div>');
-                        
-                        $('.user-info .center').append('<div>:</div>');
-                        $('.user-info .center').append('<div>:</div>');
-                        $('.user-info .center').append('<div>:</div>');
-                        $('.user-info .center').append('<div>:</div>');
-                        
-                        $('.user-info .right').append('<div>'+resultt.api_endpoint+'</div>');
-                        $('.user-info .right').append('<div>'+resultt.org+'</div>');
-                        $('.user-info .right').append('<div>'+resultt.space+'</div>');
-                        $('.user-info .right').append('<div>'+resultt.username+'</div>');
-                    },
-                    error: function(xhr, status, error) {
-                        console.log(error);
-                    },
-                    dataType: 'json'
-                });
-            }
-        },
-        error: function(xhr, status, error) {
-            console.log(error);
-        },
-        dataType: 'json'
-    });
+    if ( username == "" ) {
+        $('.response-body').html('This username was not found. Please enter a valid shortname.');
+    } else {
+        $('.response-body').html('Please wait ...');
+        $.ajax({
+            type: 'GET',
+            url: getURL,
+            success: function(result){
+                if(!result.isLDAPUserPresent) {
+                    $('.response-body').html('This username was not found. Please enter a valid shortname.');
+                } else {
+                    $.ajax({
+                        type: 'PUT',
+                        url: putURL,
+                        success: function(resultt) {
+                            $('.response-body').html('Your user was created!<br>');
+                            $('.response-body').append('<div class="user-info">'+
+                                                            '<div class="left"></div>'+
+                                                            '<div class="center"></div>'+
+                                                            '<div class="right"></div>'+
+                                                       '</div>');
+
+                            $('.user-info .left').append('<div>API endpoint</div>');
+                            $('.user-info .left').append('<div>Org</div>');
+                            $('.user-info .left').append('<div>Space</div>');
+                            $('.user-info .left').append('<div>Username</div>');
+
+                            $('.user-info .center').append('<div>:</div>');
+                            $('.user-info .center').append('<div>:</div>');
+                            $('.user-info .center').append('<div>:</div>');
+                            $('.user-info .center').append('<div>:</div>');
+
+                            $('.user-info .right').append('<div>'+resultt.api_endpoint+'</div>');
+                            $('.user-info .right').append('<div>'+resultt.org+'</div>');
+                            $('.user-info .right').append('<div>'+resultt.space+'</div>');
+                            $('.user-info .right').append('<div>'+resultt.username+'</div>');
+                        },
+                        error: function(xhr, status, error) {
+                            console.log(error);
+                        },
+                        dataType: 'json'
+                    });
+                }
+            },
+            error: function(xhr, status, error) {
+                console.log(error);
+            },
+            dataType: 'json'
+        });
+    }
 
 });
